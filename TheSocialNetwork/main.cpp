5f9731efaadf6507fea87be06b0b4134fa54114b
JohnnyLoud <johnloudaros@hotmail.com>
2019-05-24 18:45:43 +0300

    Initial Commit

---
+++ TheSocialNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TheSocialNetwork
+//
+//  Created by Γιάννης Λουδάρος on 24/05/2019.
+//  Copyright © 2019 Γιαννης Λουδαρος. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
