4e42e14659f6662ed0facb03cac96f52459228c7
JohnnyLoud <johnloudaros@hotmail.com>
2019-05-26 03:55:20 +0300

    Δημιούργησα αρχεία για να βάζουμε τις εξαιρέσεις

---
+++ TheSocialNetwork/Exceptions.cpp
@@ -0,0 +1,9 @@
+//
+//  Exceptions.cpp
+//  TheSocialNetwork
+//
+//  Created by Γιάννης Λουδάρος on 26/05/2019.
+//  Copyright © 2019 Γιαννης Λουδαρος. All rights reserved.
+//
+
+#include "Exceptions.hpp"
---
+++ TheSocialNetwork/Exceptions.hpp
@@ -0,0 +1,14 @@
+//
+//  Exceptions.hpp
+//  TheSocialNetwork
+//
+//  Created by Γιάννης Λουδάρος on 26/05/2019.
+//  Copyright © 2019 Γιαννης Λουδαρος. All rights reserved.
+//
+
+#ifndef Exceptions_hpp
+#define Exceptions_hpp
+
+#include <stdio.h>
+
+#endif /* Exceptions_hpp */
